84cae615a9f059d544870b49993e96bd92b62840
ahmadrosid <alahmadrosid@gmail.com>
2024-11-03 14:01:57 +0700

    Update copy-icon component version
    - Upgrade the copy-icon component from version 1.0.1 to 1.0.2
    - This update includes bug fixes and improvements to the copy-icon functionality

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -76,16 +76,7 @@
                     <div class="flex gap-3 items-center text-neutral-500 px-3">
                         <span wire:key="char-count" class="text-xs">{{ strlen($post) }}/3000 characters</span>
                         <div class="w-px bg-neutral-100 h-6"></div>
-                        <button @click="navigator.clipboard.writeText($wire.output);"
-                            class="flex items-center gap-1 text-neutral-600 text-xs hover:text-neutral-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="size-3">
-                                <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
-                                <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
-                            </svg>
-                            Copy
-                        </button>
+                        <copy-icon onclick="navigator.clipboard.writeText($wire.output);" class="!inline-block"></copy-icon>
                     </div>
                 </div>
                 <div class="prose prose-sm max-w-none p-3 rounded mb-3 bg-neutral-100 mx-3" wire:stream="output">
