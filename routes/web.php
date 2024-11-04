5cf7f399b15ee2a4c8efd030f897ae694cb3779d
ahmadrosid <alahmadrosid@gmail.com>
2024-11-04 16:05:04 +0700

    Implement environment-based route configuration
    - Wrap all routes in a closure function
    - Check the application environment and apply middleware based on the environment
    - In production, wrap the routes in the 'auth' middleware group
    - In non-production environments, use the routes without any middleware

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,21 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index', ['component' => 'create-post', 'title' => 'Create LinkedIn Post'])->name('create');
-Route::view('/create', 'index', ['component' => 'create-post', 'title' => 'Create LinkedIn Post']);
-Route::view('/copy-cat', 'index', ['component' => 'copy-cat', 'title' => 'Copy what works!']);
-Route::view('/history', 'index', ['component' => 'post-history', 'title' => 'Post history'])->name('history');
+$routes = function() {
+    Route::view('/', 'index', ['component' => 'create-post', 'title' => 'Create LinkedIn Post'])->name('create');
+    Route::view('/create', 'index', ['component' => 'create-post', 'title' => 'Create LinkedIn Post']);
+    Route::view('/copy-cat', 'index', ['component' => 'copy-cat', 'title' => 'Copy what works!']);
+    Route::view('/history', 'index', ['component' => 'post-history', 'title' => 'Post history'])->name('history');
 
-Route::get('/post/{post}', function (Post $post) {
-    return view('detail', ['post' => $post]);
-})->name('post.detail');
+    Route::get('/post/{post}', function (Post $post) {
+        return view('detail', ['post' => $post]);
+    })->name('post.detail');
 
-Route::get('/post/edit/{post}', function (Post $post) {
-    return view('index', ['component' => 'edit-post', 'post' => $post]);
-})->name('post.edit');
+    Route::get('/post/edit/{post}', function (Post $post) {
+        return view('index', ['component' => 'edit-post', 'post' => $post]);
+    })->name('post.edit');
+};
+
+app()->environment('production') 
+    ? Route::middleware('auth')->group($routes) 
+    : Route::group([], $routes);
